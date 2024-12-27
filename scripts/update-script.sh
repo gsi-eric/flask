e3e237c909fdda02d8309312e4c4ba49059c91b0
Eric Torres <eric.torres@alivi.com>
2024-12-27 07:35:44 +0100

    feat: Added the cd to deploy and update the docker image

---
+++ scripts/update-script.sh
@@ -0,0 +1,5 @@
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 273354652660.dkr.ecr.us-east-1.amazonaws.com/flask
+docker image prune -af
+docker pull 273354652660.dkr.ecr.us-east-1.amazonaws.com/flask:latest
+docker-compose down 
+docker-compose up -d
