1e85fb147164cde9073322ea0db18163ceda0cf7
Eric Torres <eric.torres@alivi.com>
2024-12-27 08:06:02 +0100

    feat: add a new path

--- scripts/update-script.sh
+++ scripts/update-script.sh
@@ -1,3 +1,4 @@
+cd /opt/flask
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 273354652660.dkr.ecr.us-east-1.amazonaws.com/flask
 docker image prune -af
 docker pull 273354652660.dkr.ecr.us-east-1.amazonaws.com/flask:latest
